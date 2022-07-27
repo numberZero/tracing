0a8b31b7a20d010e28b8e6161de236e5e974159c
numzero <numzer0@yandex.ru>
2022-07-27 21:39:39 +0300

    Трассировка того, что изначально предполагалось =)

--- prog2.cxx
+++ prog2.cxx
@@ -21,11 +21,33 @@ inline static float sqr(float x) {
 	return x * x;
 }
 
-static mat2 metric(vec2 pos) { // с нижними индексами!
+inline static float smoothstep(float x) {
+	return 3 * x * x - 2 * x * x * x;
+}
+
+inline static float box(float val, vec2 range, float pad) {
+	float slope1 = 1.0f + (val - range.x) / pad;
+	float slope2 = 1.0f - (val - range.y) / pad;
+	float lin = std::min(slope1, slope2);
+	return smoothstep(glm::clamp(lin, 0.0f, 1.0f));
+}
+
+static const float box_scale = 3.0f;
+static const vec2 box_a = {-0.3f, -3.0f};
+static const vec2 box_b = {0.3f, 3.0f};
+static const vec2 box_m = {0.1f, 0.5f};
+
+static mat2 halfmetric(vec2 pos) {
+	float s = box(pos.x, {box_a.x, box_b.x}, box_m.x) * box(pos.y, {box_a.y, box_b.y}, box_m.y);
 	return {
 		{1.0f, 0.0f},
-		{0.0f, sqr(pos.x)},
+		{0.0f, pow(box_scale, -s)},
 	};
+}
+
+static mat2 metric(vec2 pos) { // с нижними индексами!
+	mat2 h = halfmetric(pos);
+	return h * h;
 }
 
 template <int N>
@@ -136,7 +158,7 @@ static std::vector<vec<N>> trace(vec<N> base, vec<N> dir, float distance, float 
 	std::vector<vec<N>> result;
 	result.reserve(steps + 1);
 	auto p = base;
-	auto v = dir;
+	auto v = glm::inverse(halfmetric(base)) * dir;
 	v /=  length(p, v);
 	result.push_back(p);
 	for (int k = 0; k < steps; k++) {
@@ -152,19 +174,44 @@ vec2 to_cartesian(vec2 polar) {
 	return {polar.x * std::cos(polar.y), polar.x * std::sin(polar.y)};
 }
 
-int main() {
-	std::vector<vec2> line;
-	for (int k = 0; k <= 1024; k++) {
-		vec2 xy = {3.0f, k / 256.0f};
-		float r = glm::length(xy);
-		line.emplace_back(r, std::atan2(xy.y, xy.x));
-	}
-	printf("4.0 = %.3f\n", length(std::span{line}));
-
-	line = trace(vec2{3.0f, 0.0f}, vec2{0.0f, 1.0f}, 4.0f);
+void draw_path(FILE *svg, char const *cls, std::span<vec2 const> line, bool close = false, int skip = 1) {
+	fprintf(svg, "<path class=\"%s\" d=\"M", cls);
+	int n = 0;
 	for (vec2 p: line) {
-		vec2 xy = to_cartesian(p);
-		printf("%.3f, %.3f <- %.3f, %.3f\n", xy.x, xy.y, p.x, p.y);
+		if (n % skip == 0)
+			fprintf(svg, " %.3f,%.3f", p.x, p.y);
+		if (!n)
+			fprintf(svg, " L");
+		n++;
 	}
-	printf("%.3f = %.3f\n", glm::distance(to_cartesian(line.front()), to_cartesian(line.back())), length(std::span{line}));
+	if (close)
+		fprintf(svg, " Z");
+	fprintf(svg, "\" />\n");
+}
+
+void draw_box(FILE *svg, char const *cls, vec2 a, vec2 b) {
+	draw_path(svg, cls, {std::vector{a, {a.x, b.y}, b, {b.x, a.y}}}, true);
+}
+
+int main() {
+	FILE *svg = fopen("out.svg", "wb");
+	fprintf(svg, "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n");
+	fprintf(svg, "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"-6 -4 12 8\">");
+	fprintf(svg, "<style>@import url(style.css);</style>");
+	for (int j = -64; j <= 64; j++) {
+		float y = j / 64.0f;
+		printf("%d: %.3f\n", j, y);
+		auto line = trace(vec2{-4.0f, 0.0f}, vec2{1.0f, y}, 10.0f);
+		draw_path(svg, "ray", {line}, false, 10);
+	}
+	for (int j = -64; j <= 64; j++) {
+		float x = j / 64.0f;
+		printf("%d: %.3f\n", j, x);
+		auto line = trace(vec2{0.0f, 0.0f}, vec2{x, 1.0f}, 10.0f);
+		draw_path(svg, "ray2", {line}, false, 10);
+	}
+	draw_box(svg, "inner", box_a, box_b);
+	draw_box(svg, "outer", box_a - box_m, box_b + box_m);
+	fprintf(svg, "</svg>");
+	fclose(svg);
 }
