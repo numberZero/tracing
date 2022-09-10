fc86df0d4113c75dc8ee49e3ea44fc3b34d71947
numzero <numzer0@yandex.ru>
2022-09-10 21:38:04 +0300

    Slight scene rework

--- prog3.cxx
+++ prog3.cxx
@@ -198,20 +198,24 @@ public:
 };
 
 static constexpr float basement_radius = 1000.0f;
+#if BEST
+static constexpr int additional_spheres = 300;
+#else
+static constexpr int additional_spheres = 30;
+#endif
 
 std::vector<Surface> surfaces = {
 // Surface const surfaces[] = {
 	{*new Sphere{{-3.0, 4.0,  3.0}, 2}, *new Metallic({0.5, 0.5, 0.6}, 0.1)},
 	{*new Sphere{{-1.0, 2.5, -1.0}, 2}, *new Metallic({0.7, 0.1, 0.1}, 0.5)},
 	{*new Sphere{{ 2.5, 3.5,  1.0}, 2}, *new Diffuse({0.3, 0.2, 0.7})},
-	{*new Sphere{{ 7.0, 9.0, -3.0}, 2}, *new Specular({0.7, 0.7, 0.7})},
-	{*new Sphere{{ 7.0, 9.0,  9.0}, 2}, *new Shiny({3.7, 1.7, 1.7})},
+	{*new Sphere{{ 7.0, 5.0,  9.0}, 2}, *new Shiny({3.7, 1.7, 1.7})},
 	{*new Sphere{{0.0, -basement_radius, 0.0}, basement_radius}, *new Diffuse({0.65, 0.65, 0.65})},
 };
 
 void prepare() {
 	rgen.seed(0x12345678);
-	for (int k = 0; k < 300; k++) {
+	for (int k = 0; k < additional_spheres; k++) {
 		float u = randd();
 		float v = randd();
 		float radius = 0.1f + 0.2f * u + 0.6f * pow(v, 10);
@@ -238,17 +242,18 @@ void prepare() {
 }
 
 static vec3 const sun_dir = normalize(vec3{100.0f, 200.0f, 100.0f});
-static float const sun_size = 0.002f;
-static vec3 const sun_color = {20.0f, 18.0f, 12.0f};
+static float const sun_size = 0.02f;
+static vec3 const sun_color = {20.0f, 16.0f, 8.0f};
 
 vec3 background(vec3 dir) {
 // 	return{};
 // 	if (dir.y < -.1)
 // 		return {0.1, 0.5, 0.0};
-	vec3 sky_color = mix(vec3{.6, .7, 1.}, vec3{.9, .9, 1.}, dir.y);
+	vec3 sky_color = mix(vec3{0.6, 0.8, 0.9}, vec3{0.6, 0.7, 1.2}, dir.y);
+// 	sky_color += .3f * mix(vec3{.5, .6, .8}, vec3{.7, .9, 1.}, dot(dir, sun_dir));
+	float sun_d = (1.f - dot(dir, sun_dir)) / sun_size;
+	sky_color += sun_color * glm::clamp(1.f - sun_d, 0.f, 1.f);
 	return sky_color;
-// 	float sun_d = (1.f - dot(dir, sun_dir)) / sun_size;
-// 	return mix(sun_color, sky_color, glm::clamp(sun_d - 1.f, 0.f, 1.f));
 }
 
 struct Hit {
