a6d530d1942593c88241ec76796fd75a803a2464
numzero <numzer0@yandex.ru>
2023-12-08 19:20:06 +0300

    Optimize a bit more memory handling

--- prog6.cxx
+++ prog6.cxx
@@ -886,9 +886,18 @@ std::vector<VisualTraceResult> trace(std::vector<TrackPoint> rays) {
 		struct Batch {
 			std::vector<int> indices;
 			std::vector<Ray> rays;
+			int size = 0;
 		};
 		std::unordered_map<Subspace const *, Batch> batches;
 
+		for (auto &&job: jobs) {
+			Batch &batch = batches[job.pt.space];
+			batch.size++;
+		}
+		for (auto &&[key, batch]: batches) {
+			batch.indices.reserve(batch.size);
+			batch.rays.reserve(batch.size);
+		}
 		for (auto &&job: jobs) {
 			Batch &batch = batches[job.pt.space];
 			batch.indices.push_back(job.at);
